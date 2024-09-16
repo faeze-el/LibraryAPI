4ea8f439866d14c5d328170b0a89fb5e58299f2d
faeze_el <fa35319@yahoo.com>
2024-09-16 22:34:27 +0330

    comment

--- src/main/java/Dotin/LibraryProject/Entity/Librarian.java
+++ src/main/java/Dotin/LibraryProject/Entity/Librarian.java
@@ -21,24 +21,24 @@ public class Librarian extends UserOld {
     @Override
     public String executeCommand(int commandIndex, String args){
         LibraryCore libraryCore = LibraryCore.getInstance();
-        switch (commandIndex) {
-            case 1:
-                if (!args.isEmpty()) {
-                    String[] argsArray = args.split(" ");
-                    int requestId = Integer.parseInt(argsArray[0]);
-                    String checkApprove = argsArray[1].toLowerCase();
-                    return libraryCore.approveRequest(requestId, checkApprove);
-                }
-                else return "Enter valid arguments";
-            case 2:
-                System.out.println("requests");
-                return libraryCore.getRequestsAsString();
-            case 3:
-                System.out.println("books");
-                return libraryCore.getBooksAsString();
-            default:
-                System.out.println("Invalid action.");
-        }
+//        switch (commandIndex) {
+//            case 1:
+//                if (!args.isEmpty()) {
+//                    String[] argsArray = args.split(" ");
+//                    int requestId = Integer.parseInt(argsArray[0]);
+//                    String checkApprove = argsArray[1].toLowerCase();
+//                    return libraryCore.approveRequest(requestId, checkApprove);
+//                }
+//                else return "Enter valid arguments";
+//            case 2:
+//                System.out.println("requests");
+//                return libraryCore.getRequestsAsString();
+//            case 3:
+//                System.out.println("books");
+//                return libraryCore.getBooksAsString();
+//            default:
+//                System.out.println("Invalid action.");
+//        }
         return "";
     }
 }
