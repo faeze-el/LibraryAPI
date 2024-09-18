4cf8bf264e755d8473c782cc91c2ed2ea8f18fca
Tahiran <aa2893054@gmail.com>
2024-09-18 06:29:34 -0700

    Working Reservation controller with database

--- src/main/java/Dotin/LibraryProject/Service/ReservationService.java
+++ src/main/java/Dotin/LibraryProject/Service/ReservationService.java
@@ -1,6 +1,7 @@
 package Dotin.LibraryProject.Service;
 
 import Dotin.LibraryProject.Entity.ReservationRequest;
+import Dotin.LibraryProject.Repository.ReservationRepositoryByDB;
 import Dotin.LibraryProject.Repository.ReservationRepositoryNoDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,23 @@ public class ReservationService {
 
     @Autowired
     private ReservationRepositoryNoDB reservationRepository;
+    @Autowired
+    private ReservationRepositoryByDB reservationRepositoryByDB;
 
     public List<ReservationRequest> getReservationRequestList() {
-        return reservationRepository.getAllReservations();
+//        return reservationRepository.getAllReservations();
+        return reservationRepositoryByDB.getAllReservations();
     }
-    public List<ReservationRequest> getReservationsByUserId(Long id) { return  reservationRepository.getReservationsById(id);}
-    public void addNewReservation(ReservationRequest res) {reservationRepository.addReservation(res);}
+    public List<ReservationRequest> getReservationsByUserId(Long id) {
+//        return  reservationRepository.getReservationsById(id);
+        return reservationRepositoryByDB.getReservationsById(id);
+    }
+    public void addNewReservation(ReservationRequest res) {
+//        reservationRepository.addReservation(res);
+        reservationRepositoryByDB.addReservation(res);
+    }
     public boolean updateReservation(Long id, String isApprove){
-        return reservationRepository.updateReservation(id, isApprove);
+//        return reservationRepository.updateReservation(id, isApprove);
+        return reservationRepositoryByDB.updateReservation(id,isApprove);
     }
 }
