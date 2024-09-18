a699a40d747cd0c5dc2cb1005e89c3bd92365dfb
Tahiran <aa2893054@gmail.com>
2024-09-18 06:42:49 -0700

    Working User controller with database

--- src/main/java/Dotin/LibraryProject/Service/UserService.java
+++ src/main/java/Dotin/LibraryProject/Service/UserService.java
@@ -2,6 +2,7 @@ package Dotin.LibraryProject.Service;
 
 import Dotin.LibraryProject.Entity.User;
 import Dotin.LibraryProject.Repository.UserRepository;
+import Dotin.LibraryProject.Repository.UserRepositoryByDb;
 import Dotin.LibraryProject.Repository.UserRepositoryNoDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,16 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepositoryNoDB userRepository;
+    @Autowired
+    private UserRepositoryByDb userRepositoryByDb;
 
     public List<User> getUsers(){
-        return userRepository.getAllUsers();
+//        return userRepository.getAllUsers();
+        return userRepositoryByDb.getAllUsers();
     }
 
     public void addNewUser(User user){
-        userRepository.addUser(user);
+//        userRepository.addUser(user);
+        userRepositoryByDb.addUser(user);
     }
 }
