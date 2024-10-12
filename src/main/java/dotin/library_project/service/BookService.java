8d898c979013eff330cd969116327e027afb68e6
faeze_el <fa35319@yahoo.com>
2024-10-12 23:19:40 +0330

    kjbb

--- src/main/java/dotin/library_project/service/BookService.java
+++ src/main/java/dotin/library_project/service/BookService.java
@@ -20,7 +20,6 @@ import java.util.Objects;
 public class BookService {
 
     private final BookRepository bookRepository;
-    private final BookValidator validator =new BookValidator();
 
 
     public BookService(@Qualifier("bookRepositoryByDb") BookRepository bookRepository ) {
